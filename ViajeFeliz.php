28104feb17080ac08e1787e24b6c30783a16d5bd
Amadesx <94643387+Amadesx@users.noreply.github.com>
2023-04-06 03:00:39 -0300

    Update ViajeFeliz.php

--- ViajeFeliz.php
+++ ViajeFeliz.php
@@ -1,6 +1,7 @@
 <?php
-// La clase viaje tiene los atributos privados $codigo, $destino, $cant_max_pasajeros y $pasajeros. $codigo y $destino son de tipo string, $cant_max_pasajeros es de tipo entero y por ultiumo $pasajeros es
-// un array que almacena la informacion de cada pasajero.
+
+// La clase viaje tiene los atributos privados $codigo, $destino, $cant_max_pasajeros y $pasajeros. $codigo y $destino son de tipo string, $cant_max_pasajeros es de tipo entero y por último $pasajeros es
+// un array que almacena la información de cada pasajero.
 class Viaje {
     private $codigo;
     private $destino;
@@ -57,7 +58,7 @@ class Viaje {
     // Este método agrega un nuevo pasajero a la lista de pasajeros, el nuevo pasajero se representa como un array asociativo con claves de "nombre", "apellido" y "dni"
     // si se agrega correctamente el método devuelve "true", sino devuelve "false"
     public function agregarPasajero($nombre, $apellido, $dni) {
-        if (count($this->pasajeros) < $this->cant_max_pasajeros) {
+        if (count($this->pasajeros) < $this->getCantMaxPasajeros()) {
             $pasajero = array(
                 "nombre" => $nombre,
                 "apellido" => $apellido,
@@ -80,18 +81,58 @@ class Viaje {
         }
         return false;
     }
-
-
-    public function __toString(){
+    // Este método recibe como parámetros el dni de un pasajero y un nuevo nombre para actualizarlo en la lista de pasajeros.
+    // La función utiliza un bucle foreach para buscar al pasajero con el dni especificado y, si lo encuentra, actualiza su nombre y devuelve true. En caso contrario, devuelve false.
+    // También en las 3 siguientes funciones, utilizo el símbolo "&" en la variable $pasajero dentro del bucle foreach para actualizar directamente el valor del arreglo de pasajeros del objeto
+    // para evitar crear una copia del arreglo de pasajeros y remplazarla por la original, ya que esta hace referencia a la variable original y cualquier cambio realizado afectará a la variable original.
+    public function modificarNombrePasajero($dni, $nuevo_nombre) {
+        foreach ($this->pasajeros as &$pasajero) {
+            if ($pasajero["dni"] == $dni) {
+                $pasajero["nombre"] = $nuevo_nombre;
+                return true;
+            }
+        }
+        return false;
+    }
+    // Lo mismo que el de arriba, pero modifica el apellido
+    public function modificarApellidoPasajero($dni, $nuevo_apellido) {
+        foreach ($this->pasajeros as &$pasajero) {
+            if ($pasajero["dni"] == $dni) {
+                $pasajero["apellido"] = $nuevo_apellido;
+                return true;
+            }
+        }   
+        return false;
+    }
+// Este método recibe como parametros el dni actual del pasajero y un nuevo dni, luego con un bucle foreach busca al pasajero con el dni especificado y si lo encuentra lo actualiza con el
+// nuevo valor y devuelve true. En caso contrario, devuelve false.
+    public function modificarDniPasajero($dni_actual, $nuevo_dni) {
+        foreach ($this->pasajeros as &$pasajero) {
+            if ($pasajero["dni"] == $dni_actual) {
+                $pasajero["dni"] = $nuevo_dni;
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    
+// Este metodo recorre el arreglo de pasajeros del objeto y contruye una cadena para cada pasajero que tiene su nombre, apellido y dni. Despues agrega cada cadena a un arreglo de cadenas usando
+// la funcion implode para conseguir la cadena final.
+    public function pasajerosString(){
         $pasajeros_arreglo = array();
         foreach ($this->pasajeros as $pasajero) {
-            $pasajeros_arreglo[] = $pasajero["nombre"]. " " . $pasajero["apellido"]. " (" . $pasajero["dni"]. ")". " ,"; 
+            $pasajeros_arreglo[] = $pasajero["nombre"]. " " . $pasajero["apellido"]. " (" . $pasajero["dni"]. ")";
+        }
+        $pasajero_string = implode(", ", $pasajeros_arreglo);
+        return $pasajero_string;
     }
-        $pasajero_string = implode(", ". $pasajero_arreglo);
-        return " Viaje a :  " .$this->destino. "  /Codigo:  " .$this->codigo. "  /Con los siguientes pasajeros: ". $pasajeros_string;
 
+
+// Accede a los atributos y al arreglo de cadenas de los pasajeros para formar un string final con todos los datos pedidos.
+    public function __toString() {
+        return "Viaje a " . $this->destino . " (Código: " . $this->codigo . ") con pasajeros: " . $this->pasajerosString();
+    }
+    
 }
-}
-// Cambiar los $this-›x por los métodos de acceso
-// Solo el método constructor accede a los atributos con  $this-›x
-// Agregar la posibilidad de modificar datos de los pasajeros (nombre, apellido, DNI)
+
