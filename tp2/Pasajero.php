4e7896d90c22043b7356c325266c7d27e14c19fb
Amadesx <94643387+Amadesx@users.noreply.github.com>
2023-04-18 02:44:38 -0300

    Update Pasajero.php

--- tp2/Pasajero.php
+++ tp2/Pasajero.php
@@ -1,52 +1,52 @@
 <?php
 
+// Objeto Pasajero
 class Pasajero {
-    
     private $nombre;
     private $apellido;
-    private $num_telefono;
+    private $telefono;
     private $dni;
 
-    public function __construct($nombre, $apellido, $num_telefono, $dni){
-        $this->nombre=$nombre;
-        $this->apellido=$apellido;
-        $this->num_telefono=$num_telefono;
-        $this->dni;
+    public function __construct($nombre, $apellido, $telefono, $dni) {
+        $this->nombre = $nombre;
+        $this->apellido = $apellido;
+        $this->telefono = $telefono;
+        $this->dni = $dni;
     }
-    
-    public function getNombre(){
+
+    public function getNombre() {
         return $this->nombre;
     }
 
-    public function getApellido(){
-        return $this->apellido;        
+    public function setNombre($nombre) {
+        $this->nombre = $nombre;
     }
 
-    public function getNum_telefono(){
-        return $this->num_telefono;
+    public function getApellido() {
+        return $this->apellido;
     }
 
-    public function getDni(){
+    public function setApellido($apellido) {
+        $this->apellido = $apellido;
+    }
+
+    public function getTelefono() {
+        return $this->telefono;
+    }
+
+    public function setTelefono($telefono) {
+        $this->telefono = $telefono;
+    }
+
+    public function getDni() {
         return $this->dni;
     }
 
-    public function setNombre($nombre){
-        $this->nombre=$nombre;
-    } 
-
-    public function setApellido($apellido){
-        $this->apellido=$apellido;
-    }
-    
-    public function setNum_telefono($num_telefono){
-        $this->num_telefono=$num_telefono;
+    public function setDni($dni) {
+        $this->dni = $dni;
     }
 
-    public function setDni($dni){
-        $this->dni=$dni;
+    public function __toString() {
+        return "- Nombre: " . $this->nombre ."\n". "- Apellido: " . $this->apellido. "\n" . "- Telefono: " . $this->telefono."\n" . "- DNI: " . $this->dni. "\n";
     }
-    public function __toString(){
-        return "Nombre: " .$this->getNombre(). "Apellido: " .$this->getApellido(). "Numero de telefono: " .$this->getNum_telefono(). "DNI: " .$this->getDni(). "\n";
-    }
-
 }
